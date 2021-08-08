9c70cdc562dc212ee1d4bd8fa95fc72d3f17160c
INFINITY00800 <72161909+INFINITY00800@users.noreply.github.com>
2021-08-09 00:25:47 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/DarkCybers/InnexiaMusic /root/innexia
+git clone https://github.com/INFINITY00800/InnexiaMusic /root/innexia
 cp innexiamusic/.env /root/innexia
 cd /root/innexia
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t innexiamusic
